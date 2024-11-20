f1f0b8ac1da725bdb3e4af5887d6d0d5154d5023
manozchhetri <156447784+manozchhetri@users.noreply.github.com>
2024-11-19 22:35:52 -0500

    appending html element to article element and again apending article to section element

--- js/json.js
+++ js/json.js
@@ -61,9 +61,13 @@ function showTopFlavors(jsonObj) {
             // add the ingredient to the UL
 
         // STEP 10h: Append each of the above HTML elements to the ARTICLE element
-        
+        article.appendChild(h2);
+    article.appendChild(image);
+    article.appendChild(pCalories); // Append the paragraph for calories
+    article.appendChild(ul);
         // STEP 10i: Append each complete ARTICLE element to the SECTION element
-        
+        section.appendChild(article);
+  
     };
 };
 // STEP 11: The instructor will edit the JSON file - refresh your page to see the updated content
